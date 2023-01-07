94c8abcb996b0e13c101649e020eaaaa9cb06be8
prabeshlama0 <prabeshlama0@gmail.com>
2023-01-07 13:12:24 -0600

    Added the toString method

--- src/Customer.java
+++ src/Customer.java
@@ -1,9 +1,18 @@
 public class Customer {
     String name;
-    Long contactNo;
-    String email;
+    Long contactNo=874837485L;
+    String email="test@gmeil.com";
 
     public void getMessage(){
         System.out.println("Get message is called");
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", contactNo=" + contactNo +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer();
         System.out.println(customer);
+        customer.getMessage();
+        customer.name="David";
+        System.out.println(customer);
     }
 }
